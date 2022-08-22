0ec64cd129a2b448e08c2689eb052b5fe52bc589
한상혁 <93487681+Alwayshyuk@users.noreply.github.com>
2022-08-22 20:11:17 +0900

    Add files via upload

---
+++ board/build/generated/sources/annotationProcessor/java/main/org/zerock/board/entity/QMember.java
@@ -0,0 +1,49 @@
+package org.zerock.board.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = 1153713545L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    public final StringPath email = createString("email");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modDate = _super.modDate;
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
---
+++ board/build/generated/sources/annotationProcessor/java/main/org/zerock/board/entity/QReply.java
@@ -0,0 +1,63 @@
+package org.zerock.board.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReply is a Querydsl query type for Reply
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReply extends EntityPathBase<Reply> {
+
+    private static final long serialVersionUID = -1343635941L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReply reply = new QReply("reply");
+
+    public final QBaseEntity _super = new QBaseEntity(this);
+
+    public final QBoard board;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modDate = _super.modDate;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> regDate = _super.regDate;
+
+    public final StringPath replyer = createString("replyer");
+
+    public final NumberPath<Long> rno = createNumber("rno", Long.class);
+
+    public final StringPath text = createString("text");
+
+    public QReply(String variable) {
+        this(Reply.class, forVariable(variable), INITS);
+    }
+
+    public QReply(Path<? extends Reply> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReply(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReply(PathMetadata metadata, PathInits inits) {
+        this(Reply.class, metadata, inits);
+    }
+
+    public QReply(Class<? extends Reply> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.board = inits.isInitialized("board") ? new QBoard(forProperty("board"), inits.get("board")) : null;
+    }
+
+}
+
