dd9e0a2683600df32f82991084b2a0d310f618ea
한상혁 <93487681+Alwayshyuk@users.noreply.github.com>
2022-08-22 19:16:47 +0900

    Add files via upload

---
+++ ex1/src/test/java/org/zerock/ex1/Ex1ApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("contextLoads...");
+	}
+
+}
